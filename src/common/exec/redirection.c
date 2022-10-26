80ecfa2b694ab3ebe185a35c8d947fd22adcfb30
nijakow <79372954+nijakow@users.noreply.github.com>
2022-10-26 23:57:43 +0200

    Extended redirections

--- src/common/exec/redirection.c
+++ src/common/exec/redirection.c
@@ -2,13 +2,43 @@
 
 #include "redirection.h"
 
-void nsh_redirection_create(struct nsh_redirection*   redir,
-                            enum nsh_redirection_type redir_type,
-                            char*                     redir_text) {
-    redir->redirection_type = redir_type;
-    redir->text             = nsh_strdup(redir_text);
+
+void nsh_redirection_create(struct nsh_redirection* redir) {
+    redir->type = nsh_redirection_type_none;
+    redir->text = NULL;
+    redir->next = NULL;
 }
 
 void nsh_redirection_destroy(struct nsh_redirection* redir) {
     nsh_free(redir->text);
+    nsh_redirection_delete(redir->next);
+}
+
+void nsh_redirection_set(struct nsh_redirection* redir, enum nsh_redirection_type type, const char* text) {
+    nsh_free(redir->text);
+    redir->type = type;
+    redir->text = nsh_strdup(text);
+}
+
+void nsh_redirection_set_next(struct nsh_redirection* redir, struct nsh_redirection* next) {
+    nsh_redirection_delete(redir->next);
+    redir->next = next;
+}
+
+struct nsh_redirection* nsh_redirection_quick_new(enum nsh_redirection_type type, const char* text) {
+    struct nsh_redirection*  redir;
+
+    redir = nsh_redirection_new();
+
+    if (redir != NULL) {
+        nsh_redirection_set(redir, type, text);
+    }
+
+    return redir;
+}
+
+void nsh_redirection_push(struct nsh_redirection* redir, struct nsh_redirection* next) {
+    while (redir->next != NULL)
+        redir = redir->next;
+    nsh_redirection_set_next(redir, next);
 }
--- src/common/exec/redirection.h
+++ src/common/exec/redirection.h
@@ -4,20 +4,23 @@
 #include "../../defs.h"
 
 enum nsh_redirection_type {
+    nsh_redirection_type_none,
     nsh_redirection_type_input_file,
     nsh_redirection_type_heredoc,
     nsh_redirection_type_output_file,
     nsh_redirection_type_append_file
 };
 
-struct nsh_redirection {
-    enum nsh_redirection_type redirection_type;
-    char*                     text;
-};
+nsh_class(nsh_redirection, {
+    enum   nsh_redirection_type  type;
+    char*                        text;
+    struct nsh_redirection*      next;
+});
 
-void nsh_redirection_create(struct nsh_redirection* redir,
-                            enum nsh_redirection_type redir_type,
-                            char*                     redir_text);
-void nsh_redirection_destroy(struct nsh_redirection* redir);
+void nsh_redirection_set(struct nsh_redirection* redir, enum nsh_redirection_type type, const char* text);
+void nsh_redirection_set_next(struct nsh_redirection* redir, struct nsh_redirection* next);
+void nsh_redirection_push(struct nsh_redirection* redir, struct nsh_redirection* next);
+
+struct nsh_redirection* nsh_redirection_quick_new(enum nsh_redirection_type type, const char* text);
 
 #endif
